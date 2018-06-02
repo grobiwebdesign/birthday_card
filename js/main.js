9321f1a41726fd1ad087a5265e67db4c9aaba1fb
Graeme Robinson <1robig68@solent.ac.uk>
2018-06-02 21:22:15 +0100

    upload
    all files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//mouse icon
+
+jQuery(document).ready(function($){
+	
+	$('a.scroll-link').click(function(e){
+		e.preventDefault();
+		$id = $(this).attr('href');
+		$('body,html').animate({
+			scrollTop: $($id).offset().top -20
+		}, 750);
+	});
+	
+});
